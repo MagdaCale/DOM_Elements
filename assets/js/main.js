54572b8e3ee823b32fdcc77643aa6763619ae19b
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-08-23 16:32:10 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,3 +60,50 @@ submit.addEventListener('click', ()=> {
     console.log(`Full name: ${vorname} ${nachname} Land: ${land}`)
 
 })
+
+
+
+/* ====================================================================== */
+
+
+
+const change2 = document.getElementById('navChange2')
+
+let aTag = document.getElementsByTagName("a")
+let checked2 = false
+
+change2.addEventListener('click', () => {    
+
+    if(checked2 === false) { 
+        //document.getElementsByTagName("a")[4].style.backgroundColor = '#f6c89f'
+        aTag[4].style.backgroundColor = '#f6c89f'
+        aTag[4].style.color = '#333'
+
+        aTag[5].style.backgroundColor = '#ffe7d1'
+        aTag[5].style.color = '#333'
+
+        aTag[6].style.backgroundColor = '#4b8e8d'
+        aTag[6].style.color = '#333'
+
+        aTag[7].style.backgroundColor = '#396362'
+        aTag[7].style.color = '#333' 
+
+        checked2 = true
+
+    }else{
+        aTag[4].style.color = '#fff'
+        aTag[4].style.backgroundColor = '#333'
+
+        aTag[5].style.color = '#fff'
+        aTag[5].style.backgroundColor = '#333'
+
+        aTag[6].style.color = '#fff'
+        aTag[6].style.backgroundColor = '#333'
+
+        aTag[7].style.color = '#fff'
+        aTag[7].style.backgroundColor = '#4CAF50';
+
+        checked2 = false
+    } 
+
+})
